8aad0933d35192348952c6b753f79df14e3d5c4b
Peter Dannemann <pdannemann@twilio.com>
2023-04-12 10:27:11 -0400

    wip

--- offsetfetch.go
+++ offsetfetch.go
@@ -66,7 +66,7 @@ type OffsetFetchPartition struct {
 // OffsetFetch sends an offset fetch request to a kafka broker and returns the
 // response.
 func (c *Client) OffsetFetch(ctx context.Context, req *OffsetFetchRequest) (*OffsetFetchResponse, error) {
-	var topics []offsetfetch.RequestTopic
+	topics := make([]offsetfetch.RequestTopic, 0, len(req.Topics))
 
 	for topicName, partitions := range req.Topics {
 		indexes := make([]int32, len(partitions))
